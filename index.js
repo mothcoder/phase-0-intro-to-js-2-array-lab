ca0dbcae159f0068327648d877eb81de79db40f9
Athena Maynard <xfifix17@gmail.com>
2024-04-28 21:58:41 -0400

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependCat(name) {
+
+    cats.unshift("Bob");
+};
+
+function destructivelyAppendCat(name) {
+    
+    cats.push("Ralph");
+};
+
+function destructivelyRemoveLastCat(name) {
+    cats.splice(2);
+    
+};
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("Milo");
+};
+
+function prependCat(name) {
+   
+    let newCat = name;
+    return  [ newCat, ...cats];
+    
+   
+
+    
+};
+
+function appendCat(name) {
+
+    let newCat= name;
+    return [...cats, newCat];
+
+    
+    
+};
+
+function removeLastCat() {
+   
+  
+    return cats.slice(0,2);
+    
+    
+    
+    
+    
+};
+
+function removeFirstCat() {
+
+    return cats.slice(1);
+    
+    
+};
